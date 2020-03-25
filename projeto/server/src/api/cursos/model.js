1c68906029c668df6ca1176d8bdc6c4e703f45dc
verzel <rafael@verzel.com.br>
2020-03-24 22:17:03 -0300

    Versão inicial Server API

---
+++ projeto/server/src/api/cursos/model.js
@@ -0,0 +1,13 @@
+const restfull = require('node-restful');
+const mongoose = restfull.mongoose;
+
+const modelo = new mongoose.Schema({
+    codigo : {type: Number, required: true},
+    descricao : { type: String, required : true},
+    cargaHoraria : {type: Number, required: true, min: 4, max: 2000},
+    preco: {type : Number, required : true},
+    categoria: {type : String, uppercase: true, 
+        enum:['INFORMATICA', 'REDES', 'ADMINISTRACAO', 'ENGENHARIA']}
+});
+
+module.exports = restfull.model('cursos', modelo);
---
+++ projeto/server/src/config/cors.js
@@ -0,0 +1,7 @@
+module.exports = (_, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    response.header("Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+}

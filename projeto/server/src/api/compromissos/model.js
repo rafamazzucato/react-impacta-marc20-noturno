e065e96d7c2e7183e5d97ff1823a7100b5e1926a
verzel <rafael@verzel.com.br>
2020-03-25 21:13:48 -0300

    settando valor default na data do compromisso

--- projeto/server/src/api/compromissos/model.js
+++ projeto/server/src/api/compromissos/model.js
@@ -1,7 +1,7 @@
 const restful = require('node-restful');
 
 const schema = new restful.mongoose.Schema({
-    data : {type: Date, required: true},
+    data : {type: Date, default: new Date()},
     descricao: {type: String, required: true},
     responsavel: {type: String, required:true},
     telefone:{type: String, required:true}

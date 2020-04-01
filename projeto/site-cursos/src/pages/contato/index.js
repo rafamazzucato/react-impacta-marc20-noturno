3a8b5e0bca2eef909dd1a40522e4633c503aa6c1
verzel <rafael@verzel.com.br>
2020-03-31 22:22:40 -0300

    conclusão tela de contato

--- projeto/site-cursos/src/pages/contato/index.js
+++ projeto/site-cursos/src/pages/contato/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Cabecalho } from '../../components/cabecalho';
+import { ContatoForm } from './form';
 
 export const ContatoScreen = _ => (
     <div className="container">
         <Cabecalho titulo="Contato" subtitulo="entre em contato conosco"/>
+        <ContatoForm/>
     </div>
 );
---
+++ projeto/site-cursos/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers} from 'redux';
+import contatoReducer from './contato';
+
+const reducers = combineReducers({
+    contato: contatoReducer
+});
+
+export default reducers;

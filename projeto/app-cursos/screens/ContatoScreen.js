a6122e77756945155567890b1b8f7f5485c92c00
verzel <rafael@verzel.com.br>
2020-04-03 20:38:52 -0300

    conclusão app

--- projeto/app-cursos/screens/ContatoScreen.js
+++ projeto/app-cursos/screens/ContatoScreen.js
@@ -1,10 +1,72 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, SafeAreaView, Text, TextInput, Dimensions, Alert } from 'react-native';
+import { Button } from 'native-base';
+import axios from 'axios';
 
-export function ContatoScreen() {
+const URL = 'http://localhost:3200/api/contatos';
+const screenWidth = Dimensions.get('screen').width;
+const screenHeight = Dimensions.get('screen').height;
+
+export function ContatoScreen(props) {
+
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [assunto, setAssunto] = React.useState('');
+
+  const limpar = () => {
+    setNome('');
+    setEmail('');
+    setAssunto('');
+  }
+
+  const enviarContato = async () => {
+    if (!nome || nome === ''
+      || !email || email === ''
+      || !assunto || assunto === '') {
+      Alert.alert(
+        'Contato',
+        'Parâmetros de entrada inválidos',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+      return;
+    }
+
+    try{
+      await axios.post(URL, {data: new Date().toUTCString(), nome, email, assunto});
+      Alert.alert(
+        'Contato',
+        'Contato Enviado com sucesso',
+        [{ text: 'Voltar para Home', onPress: () => props.navigation.navigate('Home')  }],
+        { cancelable: false }
+      );
+    }catch(error){
+      console.log(error);
+      Alert.alert(
+        'Contato',
+        'Ocorreu erro ao enviar contato',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+    }
+  }
+
   return (
     <View style={styles.container}>
-      
+      <SafeAreaView>
+        <Text style={styles.texto}>Envie seu contato</Text>
+        <TextInput style={styles.item} placeholder="Digite seu nome"
+          value={nome} onChangeText={setNome}></TextInput>
+        <TextInput style={styles.item} placeholder="Digite seu email"
+          value={email} onChangeText={setEmail}></TextInput>
+        <TextInput style={[styles.item, styles.area]} placeholder="Escreva aqui o assunto de seu interesse"
+          value={assunto} onChangeText={setAssunto}></TextInput>
+
+        <View style={[styles.containerButtons, styles.row]}>
+          <Button primary style={styles.button} onPress={enviarContato}><Text style={[styles.textButton, styles.textPrimary]}>Enviar Contato</Text></Button>
+          <Button light style={styles.button} onPress={limpar}><Text style={[styles.textButton]}>Limpar</Text></Button>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,5 +75,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
+  containerButtons: {
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'space-around',
+  },
+  button: {
+    width: screenWidth * 0.3,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textButton: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  textPrimary: {
+    color: '#fff'
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  conteudo: {
+    fontSize: 15,
+    color: "#0000ff"
+  },
+  texto: {
+    alignItems: "center",
+    fontSize: 30,
+    margin: 20,
+    color: '#7087FF',
+  },
+  item: {
+    width: screenWidth * 0.9,
+    alignItems: "center",
+    marginVertical: '3%',
+    padding: 20,
+    borderColor: '#669DE8',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  area: {
+    height: screenHeight * 0.15
+  }
 });
--- projeto/app-cursos/screens/CursosScreen.js
+++ projeto/app-cursos/screens/CursosScreen.js
@@ -1,39 +1,63 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, RefreshControl } from 'react-native';
 import axios from 'axios';
 
 const URL = 'http://localhost:3200/api/cursos';
 
 export function CursosScreen() {
   const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
 
   const listarCursos = async () => {
+    setLoading(true);
     const response = await axios.get(URL);
     setData(response.data);
+    setLoading(false);
   }
 
   React.useEffect(() => {
     listarCursos();
-  }, [listarCursos])
+  }, [])
 
   return (
     <View style={styles.container}>
-      <View>
-        <SafeAreaView>
-          <Text style={styles.texto}>Lista de Cursos</Text>
-          <FlatList
-            data={data}
-            renderItem={({ item }) => {
-              return (
-                <View style={styles.item}>
-                  <Text style={styles.conteudo}>
-                    {item.descricao}</Text>
-                </View>)
-            }}
-            keyExtractor={(item, index) => index.toString()}
-          />
-        </SafeAreaView>
-      </View>
+      <SafeAreaView>
+        <Text style={styles.texto}>Lista de Cursos</Text>
+        <FlatList
+          data={data}
+          refreshControl={
+            <RefreshControl
+              refreshing={loading}
+              onRefresh={() => listarCursos()}
+            />
+          }
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.item}>
+                <View style={styles.row}>
+                  <Text>Código: </Text>
+                  <Text style={styles.conteudo}>{item.codigo}</Text>
+                </View>
+                <View style={styles.row}>
+                  <Text>Descrição: </Text><Text style={styles.conteudo}>{item.descricao}</Text>
+                </View>
+                <View style={styles.row}>
+                  <Text>Carga Horária: </Text>
+                  <Text style={styles.conteudo}>{item.cargaHoraria}</Text>
+                  <Text>h</Text>
+                </View>
+                <View style={styles.row}>
+                  <Text>Preço: R$</Text><Text style={styles.conteudo}>{item.preco}</Text>
+                </View>
+                <View style={styles.row}>
+                  <Text>Categoria: </Text><Text style={styles.conteudo}>{item.categoria}</Text>
+                </View>
+              </View>
+            )
+          }}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </SafeAreaView>
     </View>
   );
 }
@@ -45,20 +69,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
+  row: {
+    flexDirection: 'row'
+  },
   conteudo: {
     fontSize: 15,
-    color: "#333333"
+    color: "#0000ff"
   },
   texto: {
     alignItems: "center",
     fontSize: 30,
     margin: 20,
-    color: '#FF0000',
-  }, item: {
+    color: '#7087FF',
+  },
+  item: {
     alignItems: "center",
-    backgroundColor: "#dcda44",
-    flexGrow: 1,
-    margin: 4,
-    padding: 20
+    backgroundColor: "#7DD6FF",
+    marginVertical: '3%',
+    padding: 20,
+    borderColor: '#669DE8',
+    borderWidth: 1,
+    borderRadius: 5,
   }
 });
